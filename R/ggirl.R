63a28fe5a0f2f541a40615a5606486b3c637c091
Jacqueline Nolis <jnolis@users.noreply.github.com>
2021-03-15 18:54:02 -0700

    Initial commit

---
+++ R/ggirl.R
@@ -0,0 +1,37 @@
+#' Create an address object
+#'
+#' This function takes string inputs and converts them into an address object that can be used to send irl art (or as a return address).
+#'
+#' @param name The name for the address
+#' @param address_line_1 The first line of the address
+#' @param address_line_2 (Optional) A second address line, such as an apartment number.
+#' @param city the city
+#' @param state (Optional) The state to send to
+#' @param postal_code The postal code (ZIP code in the US)
+#' @param country The 2-character [ISO-1366 code](https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes) for the country. Non-US shipping is experimental!
+#'
+#' @examples
+#' send_address <- address(name = "RStudio", address_line_1 = "250 Northern Ave", city = "Boston", state = "MA", postal_code = "02210", country = "US")
+#'
+#' @export
+address <- function(name,
+                    address_line_1,
+                    address_line_2 = NULL,
+                    city,
+                    state = NULL,
+                    postal_code,
+                    country){
+  address_set <- list(name = name,
+                      address_line_1 = address_line_1,
+                      address_line_2 = address_line_2,
+                      city = city,
+                      state = state,
+                      postal_code = postal_code,
+                      country = country)
+  # Check country is valid
+  if (!is.character(country) || nchar(country) != 2){
+    stop("Country must be a 2-character ISO-1366 code (https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes)")
+  }
+
+  structure(address_set, class="ggirl_address")
+}
---
+++ R/ggpostcard.R
@@ -0,0 +1,95 @@
+#' Order postcards of your ggplot!
+#'
+#' This function takes a ggplot2 output and will send postcards of it for you!
+#' Running this function will bring you to a webpage to confirm the order and submit it.
+#' _No order will be submitted until you explicitly approve it._
+#'
+#'
+#' @param plot the plot to put on the front of the postcard
+#' @param contact_email email address to send order updates
+#' @param return_address the return address for the postcard (required)
+#' @param messages either a message to use with all of the recipients, or a list of messages of the same length as the list of addresses (one for each address).
+#' @param send_addresses either a result of the "address()" function, or a list of results of the "address()" function.
+#' @param surver_url (development tool, do not adjust)
+#' @param ... other options to pass to ggsave when turning the plot into an image for the front of the postcard
+#' @examples
+#' library(ggplot2)
+#' return_address <- address(name = "Jacqueline Nolis", address_line_1 = "111 North St",
+#'                           city = "Seattle", state = "WA",
+#'                           postal_code = "11111", country = "US")
+#' contact_email <- "fakeemailforreal@gmail.com"
+#' send_address_1 <- address(name = "Fake Personname", address_line_1 = "250 North Ave",
+#'                           city = "Boston", state = "MA",
+#'                           postal_code = "22222", country = "US")
+#' send_address_2 <- address(name = "Anotherfake personname",
+#'                           address_line_1 = "15 NE 36th St", address_line_2 = "Apt 4",
+#'                           city = "Redmond", state = "WA",
+#'                           postal_code = "33333", country = "US")
+#'
+#' plot <- ggplot(data.frame(x=1:10, y=runif(10)),aes(x=x,y=y))+geom_line()+geom_point()
+#'
+#' # send to one recipient
+#' ggpostcard(plot, contact_email, return_address, messages = "An example postcard", send_addresses = send_address_1)
+#'
+#' # send the same message to multiple recipients
+#' ggpostcard(plot, contact_email, return_address, messages = "An example postcard", send_addresses = list(send_address_1, send_address_2))
+#'
+#' # send different messages to multiple recipients
+#' ggpostcard(plot, contact_email, return_address, messages = c("message for sender 1","message for sender 2"), send_addresses = list(send_address_1, send_address_2))
+#' @export
+ggpostcard <- function(plot=last_plot(), contact_email, return_address, messages, send_addresses, server_url = NULL, ...){
+  if(inherits(send_addresses,"ggirl_address") && length(messages) == 1){
+    # Single recipient
+    messages_and_send_addresses <-
+      list(list(message = messages, send_address = send_addresses))
+  } else if(all(sapply(send_addresses,function(x) inherits(x,"ggirl_address")))){
+    # List of recipients
+    if(length(messages) == 1){
+      # recycle the message
+      messages_and_send_addresses <-
+        lapply(send_addresses, function(x) list(message = messages, send_address = x))
+    } else {
+      messages_and_send_addresses <- mapply(function(message, send_address) list(message = message, send_address = send_address),
+                                            messages, send_addresses, SIMPLIFY = FALSE)
+      messages_and_send_addresses <- unname(messages_and_send_addresses)
+    }
+  }
+
+  version <- packageDescription("ggirl", fields = "Version")
+  if(is.na(version)){
+    version <- "0.0.0"
+  }
+
+  if(is.null(server_url)){
+    server_url <- "https://skyetetra.shinyapps.io/ggirl-server"
+  }
+
+  temp_png <- tempfile(fileext = ".png")
+  on.exit({file.remove(temp_png)}, add=TRUE)
+  postcard_width_px = 2480
+  postcard_height_px = 1748
+  postcard_dpi = 300
+  ggplot2::ggsave(filename = temp_png, plot=plot, width = postcard_width_px/postcard_dpi, height = postcard_height_px/postcard_dpi, dpi = postcard_dpi, ...)
+  raw_plot <- readBin(temp_png, "raw", file.info(temp_png)$size)
+
+  data <- list(
+    type = "postcard",
+    contact_email = contact_email,
+    raw_plot = raw_plot,
+    return_address = return_address,
+    messages_and_send_addresses = messages_and_send_addresses,
+    version = version
+  )
+
+  zz <- rawConnection(raw(0), "r+")
+  on.exit({close(zz)}, add=TRUE)
+  saveRDS(data, zz)
+  seek(zz, 0)
+
+  response <- httr::POST(paste0(server_url, "/upload"),
+                   body = rawConnectionValue(zz),
+                   httr::content_type("application/octet-stream"))
+
+  token <- httr::content(response, as="text", encoding="UTF-8")
+  browseURL(paste0(server_url,"/postcard?token=",token))
+}
